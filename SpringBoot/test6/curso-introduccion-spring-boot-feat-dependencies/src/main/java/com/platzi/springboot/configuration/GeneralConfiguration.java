20f572a537cb504abc0a3b440f84cb66dd54ab44
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-09-04 15:34:12 -0500

    datasource created

--- SpringBoot/test6/curso-introduccion-spring-boot-feat-dependencies/src/main/java/com/platzi/springboot/configuration/GeneralConfiguration.java
+++ SpringBoot/test6/curso-introduccion-spring-boot-feat-dependencies/src/main/java/com/platzi/springboot/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.platzi.springboot.bean.MyBeanWithPropertiesImplement;
 import com.platzi.springboot.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -26,4 +29,13 @@ public class GeneralConfiguration {
         return new MyBeanWithPropertiesImplement(name, apellido);
     }
 
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:test");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
 }
